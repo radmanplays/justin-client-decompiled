9ac3bd3bf19f77dac1a969c4845b11b7f0ab4cb2
radmanplays <radmanplays@users.noreply.github.com>
2024-10-03 06:41:57 +0000

    Apply automatic changes

---
+++ out/EaglerForge-hnqxs7fj.js
@@ -0,0 +1,8 @@
+// src/Main.ts
+ModAPI.meta.title("Justin v2");
+ModAPI.meta.credits("Murturtle");
+ModAPI.meta.description("Press right shift ;)");
+ModAPI.require("player");
+ModAPI.require("network");
+ModAPI.require("settings");
+ModAPI.require("world");
