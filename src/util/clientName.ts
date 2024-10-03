5e55b3d5315fe09c7f0b52108e71ccfa368e7622
radmanplays <95340057+radmanplays@users.noreply.github.com>
2024-10-03 06:24:05 +0000

    add something

---
+++ src/util/clientName.ts
@@ -0,0 +1,16 @@
+function hexToRgb(hexColor: string) {
+    var match = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexColor);
+    return match 
+      ? { r: parseInt(match[1], 16), g: parseInt(match[2], 16), b: parseInt(match[3], 16) }
+      : { r: 255, g: 255, b: 255 };
+  }
+  
+  export var clientName: string = "Justin Client v2";
+  export var color: string = "#00c3ff";
+  export var rgbColor = hexToRgb(color);
+  export var normalizedRgb = {
+    r: Math.round((rgbColor.r / 255 + Number.EPSILON) * 100) / 100,
+    g: Math.round((rgbColor.g / 255 + Number.EPSILON) * 100) / 100,
+    b: Math.round((rgbColor.b / 255 + Number.EPSILON) * 100) / 100
+  };
+  
